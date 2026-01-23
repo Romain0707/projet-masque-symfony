23017fc496dd2f7a7c18ba98d9f6fed8ffff719a
Romain Demaiz <vipdemaiz@gmail.com>
2026-01-23 10:04:46 +0100

    [NEW] Added responsive for mobile

--- assets/app.js
+++ assets/app.js
@@ -6,3 +6,11 @@
  * which should already be in your base.html.twig.
  */
 import './styles/app.scss';
+
+
+const burger = document.querySelector('.burger');
+const navMobile = document.querySelector('.nav-mobile');
+
+burger.addEventListener('click', () => {
+    navMobile.style.display = navMobile.style.display === 'flex' ? 'none' : 'flex';
+});
